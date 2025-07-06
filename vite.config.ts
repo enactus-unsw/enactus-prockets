4c0508e8fcb45bf17f3d13f4bb4dc24c2cdd5846
Amy <z5481175@ad.unsw.edu.au>
2025-07-06 23:58:09 +1000

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    base: '/enactus-prockets/',
     plugins: [react()],
     server: {
       port: 3000,
